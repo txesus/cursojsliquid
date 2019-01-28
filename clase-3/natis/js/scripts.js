efacb67c2bcad46a0c8eac90be4313b5ad46ddb5
Natiaz <natalia.diaz@tecnilogica.com>
2019-01-28 13:38:30 +0100

    Working

--- clase-3/natis/js/scripts.js
+++ clase-3/natis/js/scripts.js
@@ -1,25 +1,25 @@
 // Empty JS for your own code to be here
 function Carrera( bananas, firer ) {
-	this.numRunners = 3;
+	numRunners = 3;
 
-	this.getMaxWidth = function( distance, randoms, winner ) {
+	getMaxWidth = function( distance, randoms, winner ) {
 		var step = randoms[(winner - 1)];
 		var floor= Math.floor(distance/step);
 
 		return step*floor; 
 	};
 
-	this.movement = function( randoms, winner ) {
+	movement = function( randoms, winner ) {
 		var mochilos 	= {};
 		var moved 		= {};
-		for (var i = 1; i < this.numRunners+1; i++) {
+		for (var i = 1; i < numRunners+1; i++) {
 			mochilos[bananas+i] = document.getElementById(bananas+i);
 			moved['left'+i] = 0;
 		}
 
 		// Total width
 		var distance 	= banana1.parentNode.clientWidth;
-		var largest 	= this.getMaxWidth( distance, randoms, winner );  
+		var largest 	= getMaxWidth( distance, randoms, winner );  
 
 	  var timer = setInterval(function() {
 			if( moved['left'+ winner ] > largest)
@@ -30,7 +30,7 @@ function Carrera( bananas, firer ) {
 	    }
 	    else
 	    {
-	    	for (var i = 1; i < this.numRunners+1; i++) {
+	    	for (var i = 1; i < numRunners+1; i++) {
 					moved['left'+i] += randoms[(i-1)];
 					mochilos[bananas+i].style.left = moved['left'+i] + 'px';
 				}
@@ -38,11 +38,11 @@ function Carrera( bananas, firer ) {
     }, 20);
 	};
 
-	this.getRandoms = function() {
+	getRandoms = function() {
 		var randoms = [];
 		var winner 	= 0;
 		var max 		= 0;
-		while(randoms.length < this.numRunners){
+		while(randoms.length < numRunners){
 	    var r 		= Math.floor(Math.random()*20) + 1;	    
 	    if(randoms.indexOf(r) === -1)
 	    {
@@ -56,19 +56,19 @@ function Carrera( bananas, firer ) {
 	}
 	
 
-	this.empezar = function() {
-		this.numRunners = document.querySelectorAll('#platanero .banana').length;
+	empezar = function() {
+		numRunners = document.querySelectorAll('#platanero .banana').length;
 
-    for (var i = 1; i < this.numRunners+1; i++) {
+    for (var i = 1; i < numRunners+1; i++) {
 			document.getElementById(bananas+i).style.left = '0px';
 		}	
 		
-		var runners = this.getRandoms();
+		var runners = getRandoms();
 
-		this.movement( runners.randoms, runners.winner );				
+		movement( runners.randoms, runners.winner );				
 	};
 
-	document.getElementById(firer).addEventListener('click', this.empezar);
+	document.getElementById(firer).addEventListener('click', empezar);
 }
 
 var MiCarrera = new Carrera('banana', 'boton');
